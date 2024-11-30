968848614a3b53cd01e61d2ce92be8dcba8658f1
Justin <18606523+jlewis200@users.noreply.github.com>
2024-11-30 07:02:41 -0500

    Day 5 part 1.

--- 5/solve.py
+++ 5/solve.py
@@ -3,64 +3,85 @@
 from collections import deque
 from math import prod
 from dataclasses import dataclass
+from itertools import repeat, chain
+
+POSITION_MODE = 0
+IMMEDIATE_MODE = 1
 
 
 @dataclass
 class Args:
 
+    ip: int
     next_ip: int
-    p_destination: int
+    instruction: int
     args: list[int] = None
+    p_destination: int = None
 
 
 def solve(program, input_queue, output_queue):
     ip = 0
 
-    print(program)
-    print(ip)
-    print()
-    breakpoint()
-
     while program[ip] != 99:
         ip = process_instruction(program, ip, input_queue, output_queue)
-        print(program)
-        print(ip)
+        print(program_str(program))
+        print(output_queue)
         print()
-        breakpoint()
+
+    assert output_queue.pop() == 5182797
     return program[0]
 
 
+def program_str(program):
+    string = "\t" + "\t".join("0123456789") + "\n"
+    string += "-" * 90
+
+    for idx, value in enumerate(program):
+
+        if idx % 10 == 0:
+            string += f"\n{idx}:\t{value}"
+        else:
+            string += f"\t{value}"
+
+    return string
+
+
 def process_instruction(program, ip, input_queue, output_queue):
-    match program[ip]:
+
+    match get_opcode(program[ip]):
 
         case 1:
-            args = get_args(program, ip, arg_count=3)
+            args = get_args(program, ip, arg_types=("in", "in", "out"))
             program[args.p_destination] = sum(args.args)
 
         case 2:
-            args = get_args(program, ip, arg_count=3)
+            args = get_args(program, ip, arg_types=("in", "in", "out"))
             program[args.p_destination] = prod(args.args)
 
         case 3:
-            args = get_args(program, ip, arg_count=1)
+            args = get_args(program, ip, arg_types=("out",))
             program[args.p_destination] = input_queue.popleft()
 
         case 4:
-            args = get_args(program, ip, arg_count=1)
-            output_queue.append(program[args.p_destination])
+            args = get_args(program, ip, arg_types=("in",))
+            output_queue.append(args.args[0])
 
     print(args)
     return args.next_ip
 
 
+def get_opcode(instruction):
+    return instruction % 100
+
+
 def get_parameter_modes(instruction):
-    modes = list(str(instruction // 100))
-
-    for mode in modes:
-        yield int(mode)
-
-    while True:
-        yield 0
+    """
+    Get the modes of an opcode's parameter.  Leading zeros are omitted from the
+    opcode, so this generator will return 0s indefinitely.
+    """
+    modes = str(instruction // 100)
+    modes = map(int, reversed(modes))
+    yield from chain(modes, repeat(POSITION_MODE))
 
 
 def get_arg(program, address, mode):
@@ -70,19 +91,40 @@ def get_arg(program, address, mode):
     return program[address]
 
 
-def get_args(program, ip, arg_count):
+def get_args(program, ip, arg_types):
+    """
+    Arg types:
+        in: possibly dereferenced
+        out: never dereferenced
 
+    In parameters are dereferenced if parameter mode is 0 (positions mode),
+    and the raw value is returned for mode 1 (immediate mode).
+
+    Out parameters are not dereferenced.
+    """
+    assert isinstance(arg_types, (tuple, list))
+
+    old_ip = ip
+    instruction = program[ip]
     args = []
+    p_destination = None
     parameter_modes = get_parameter_modes(program[ip])
 
-    while len(args) < arg_count:
+    for arg_type in arg_types:
         ip += 1
         parameter_mode = next(parameter_modes)
-        args.append(get_arg(program, ip, parameter_mode))
+
+        if arg_type == "in":
+            args.append(get_arg(program, ip, parameter_mode))
+
+        elif arg_type == "out":
+            p_destination = get_arg(program, ip, IMMEDIATE_MODE)
 
     return Args(
+        ip=old_ip,
         next_ip=ip + 1,
-        p_destination=args.pop(),
+        instruction=instruction,
+        p_destination=p_destination,
         args=args,
     )
 
